67379b488b6573e556e3c8614006589384a45730
Nickleous <nickleous@gmail.com>
2021-12-12 13:09:12 -0600

    Chapter 3 work

---
+++ Chapter3/BlanchardCh3Prog2.cpp
@@ -0,0 +1,17 @@
+//
+//  BlanchardCh3Prog2.cpp
+//
+//  Created by Nick Blanchard on 9/6/21.
+//
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+
+    std::cout << "Hello, World!\n";
+    
+    
+    return 0;
+}
